c6f6ffd2cb47f169be4252dc11c1e96297b0ed2b
himanshu <himanshu@bytive.in>
2024-12-09 17:44:16 +0530

    code updated for fragment issue

--- android/app/src/main/java/com/sipfund/MainActivity.kt
+++ android/app/src/main/java/com/sipfund/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.octrax.sipfund
 
+import android.os.Bundle // Required for onCreate
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,16 +8,23 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "sipfund"
+    /**
+     * Override onCreate to pass null to avoid fragment recreation issues.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null) // Pass null to avoid fragment recreation issues
+    }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "sipfund"
+
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flag [fabricEnabled].
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import { AppRegistry } from 'react-native';
+import { enableScreens } from 'react-native-screens'; // Import the function
 import App from './App'; // Adjust the path if necessary
 import { name as appName } from './app.json';
+// import messaging from '@react-native-firebase/messaging';
 
+// Enable react-native-screens for better performance
+enableScreens();
+
+// Handle background messages
+// messaging().setBackgroundMessageHandler(async remoteMessage => {
+//     console.log('Message handled in the background!', remoteMessage);
+// });
+
+// Register the main application
 AppRegistry.registerComponent(appName, () => App);
